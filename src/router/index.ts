e0a5d0dfdd30e117699c2466f3318a9b3e756ce5
balrumeys <106353330+balrumeys@users.noreply.github.com>
2023-01-31 20:17:21 +0300

    vue3,nuxt3 ile vans clone

---
+++ src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/kadin",
+      name: "kadin",
+      component: () => import("../views/kadin.vue"),
+    },
+    {
+      path: "/erkek",
+      name: "erkek",
+      component: () => import("../views/erkek.vue"),
+    },
+    {
+      path: "/cocuk",
+      name: "cocuk",
+      component: () => import("../views/cocuk.vue"),
+    },
+    {
+      path: "/kayakkabilar",
+      name: "kayakkabilar",
+      component: () => import("../views/kayakkabilar.vue"),
+    },
+  ],
+});
+
+export default router;
